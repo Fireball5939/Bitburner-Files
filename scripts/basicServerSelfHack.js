beec0008d8b1efd0e67b8e6079563944e657eba3
FemboyFireball <111195856+Fireball5939@users.noreply.github.com>
2024-05-15 13:40:22 -0500

    Reworked serverSetup.js, fixed basicServerSelfHack Reworked serverSetup.js to make use of the server object of each target server rather than NetScript functions such as ns.hasRootAccess() and such, this rework also added the functionality of not needing to copy and run a script for recursion I also fixed basicServerSelfHack.js so it actually resets the securityReset variable upon weakening the server (Note, securityReset is intended as mostly a setup time speed up)

--- scripts/basicServerSelfHack.js
+++ scripts/basicServerSelfHack.js
@@ -11,13 +11,14 @@ export async function main(ns) {
         if (serverMaxMoney != ns.getServerMoneyAvailable(host)) {
             await ns.grow(host);
             securityReset++
-            ns.print(securityReset);
+            ns.print("This is grow #" + securityReset + ", security reset is at 15");
             if (securityReset > 15) {
 
-                while (ns.getServerSecurityLevel() > serverMinSecurity) {
+                while (ns.getServerSecurityLevel(host) > serverMinSecurity) {
                     await ns.weaken(host);
 
                 }
+                securityReset = 0;
             }
         }
         else {
--- scripts/serverSetup.js
+++ scripts/serverSetup.js
@@ -1,154 +1,113 @@
 /** @param {NS} ns */
 export async function main(ns) {
 
-  /*
+    /* A list of every used NetScript function, their descriptions, and their Ram costs
+    The format is this, ns.someFunction(arg1, arg2, ..., argX) [Ram cost] description
+    Any argument with a ? after it dictates that it is optional
 
-    This is a list of every function used, what they do, and what the input parameters are. If a parameter is optional it will be listed as such in the format of (parameter1, parameter2?, parameter3?)
+    ns.getServer(host?) [2 GB] Returns the server object (Which has info such as the server's name, weather each port buster has been used or not, its minimum security level, maximum money, and so on.) of the given host. The host argument must be a string of a valid server, and if left empty default to the server the script is running on.
+    ns.scan(host?) [0.2 GB] Returns an array of every server host name that can be directly connected to from the machine, host (Not considering backdoors). The host argument must be a string of a valid server, and if left empty default to the server the script is running on.
+    ns.fileExists(filename, host?) [0.1 GB] Returns true or false depending on if the given file, filename, exists on the machine, host. The filename argument must be the full filepath of a script (ex. "/scripts/serversetup.js"). The host argument must be a string of a valid server, and if left empty default to the server the script is running on.
+    ns.getHackingLevel(host) [0.05 GB] Returns the player's current hacking level.
+    ns.brutessh(host) [0.05 GB] Runs BruteSSH.exe on the machine, host. The host argument must be a string of a valid server.
+    ns.ftpcrack(host) [0.05 GB] Runs FTPCrack.exe on the machine, host. The host argument must be a string of a valid server.
+    ns.relaysmtp(host) [0.05 GB] Runs relaySMTP.exe on the machine, host. The host argument must be a string of a valid server.
+    ns.httpworm(host) [0.05 GB] Runs HTTPWorm.exe on the machine, host. The host argument must be a string of a valid server.
+    ns.sqlinject(host) [0.05 GB] Runs SQLInject.exe on the machine, host. The host argument must be a string of a valid server.
+    ns.nuke(host) [0.05 GB] Runs NUKE.exe on the machine, host. The host argument must be a string of a valid server.
 
+    KNOWN BUGS: The script currently only works on servers directly next to the host server due to the push pushing an object rather than strings.
+    */
 
-    ns.nuke(host); This will run NUKE.exe on the target machine 'host' and gain root access if enough ports are open, the 'host' value must be a string with the name of a valid server (n00dles, hong-fang-tea, ect). This will return nothing.
+    //Declare the needed variables
+    let serverArray = ns.scan();
+    let currentServer = 0;
 
-    ns.sqlinject(host); This will run SQLInject.exe on the target machine 'host' and open a port for NUKE.exe, the 'host' value must be a string with the name of a valid server (n00dles, hong-fang-tea, ect). This will return nothing.
+    let bruteSSH = false;
+    let ftpCrack = false;
+    let relaySMTP = false;
+    let httpWorm = false;
+    let sqlInject = false;
 
-    ns.httpworm(host); This will run HTTPWorm.exe on the target machine 'host' and open a port for NUKE.exe, the 'host' value must be a string with the name of a valid server (n00dles, hong-fang-tea, ect). This will return nothing.
+    let completedServers = [];
 
-    ns.relaysmtp(host); This will run RelaySMTP.exe on the target machine 'host' and open a port for NUKE.exe, the 'host' value must be a string with the name of a valid server (n00dles, hong-fang-tea, ect). This will return nothing.
-
-    ns.ftpcrack(host); This will run FTPCrack.exe on the target machine 'host' and open a port for NUKE.exe, the 'host' value must be a string with the name of a valid server (n00dles, hong-fang-tea, ect). This will return nothing.
-
-    ns.brutessh(host); This will run BruteSSH.exe on the target machine 'host' and open a port for NUKE.exe, the 'host' value must be a string with the name of a valid server (n00dles, hong-fang-tea, ect). This will return nothing.
-
-    ns.hasrootaccess(host); This will check to see if the player has root access on the target machine 'host' and return true if you do, and false otherwise, the 'host' value must be a string with the name of a valid server (n00dles, hong-fang-tea, ect). This will return a boolean.
-
-    ns.getservermaxram(host); This will check how much total ram the target server 'host' has and return the value as a number, the 'host' value must be a string with the name of a valid server (n00dles, hong-fang-tea, ect). This will return an integer.
-
-    ns.gethackinglevel(); This will get the current hacking level of the player and return the value as a number. This will return an integer.
-
-    ns.getservernumportsrequired(host); This will get the number of open ports the target server 'host' needs for root access to be gained and return the value as a number,the 'host' value must be a string with the name of a valid server (n00dles, hong-fang-tea, ect). This will return an integer.
-
-    ns.getscriptram(script, host?); This will get the amount of ram the target script 'script' uses with one thread and returns it as a number, the 'script' value must be a string for the full path of a file, example "/scripts/serverSetup.js", the 'host' value is optional and if given will check the file at the target server 'host', otherwise defaulting to the server the script is running on, the 'host' value must be a string with the name of a valid server (n00dles, hong-fang-tea, ect). This will return an integer.
-
-    ns.getserverrequiredhackinglevel(host); This will get the required hacking level the player needs to run ns.hack(), ns.grow(), and ns.weaken() on the target server 'host' and return it as a number, the 'host' value must be a string with the name of a valid server (n00dles, hong-fang-tea, ect). This will return an integer.
-
-    ns.fileexists(filename, host?); This will check to see if the target file 'filename' exists on the target machine 'host' and return true if it exists, and false otherwise, the 'host' value is optional and if given will check for the file at the target server 'host', otherwise defaulting to the server the script is running on, the 'host' value must be a string with the name of a valid server (n00dles, hong-fang-tea, ect). This will return a boolean.
-
-    ns.scan(host?); This will return an array with every server that can be directly connected to from the 'host' machine, the 'host' value is optional and if given will check what servers can be connected to from the 'host' server, otherwise defaulting to the current server, the 'host' value must be a string with the name of a valid server (n00dles, hong-fang-tea, ect). This will return an array of strings.
-
-    ns.killall(host?, safetyguard?); This will kill all processes on the target server 'host' and returns true if scripts were killed, and false otherwise, the 'host' value is optional and if given will dictate what server to kill all processes on, the 'safetyguard' value is optional and if given will either skip or not skip the current process from the killall function. The 'host' value must be a string with the name of a valid server (n00dles, hong-fang-tea, ect), the 'safetyguard' value must be a boolean. This will return a boolean.
-
-    ns.exec(script, hostname, threadoroptions?, arg0?, arg1?, ..., argX?); This will run the target script 'script' on the target server 'hostname' with the given number of threads 'threadoroptions' or number of threads in the given runoptions interface 'threadoroptions' and passes any other arguments to that script 'ar0, arg1, ..., argX', it then returns the PID of that process, and 0 if it failed. The 'script' value must be a valid full filepath given as a string (Example, '/scripts/serverSetup.js') that exists on the target server 'hostname', the 'hostname' value must be a valid server name given as a string (Example, 'n00dles', 'hong-fang-tea', ect), the 'threadoroptions' value must be either a number or a runoptions object, the 'arg's given to the script must be valid arguments (Numbers, Strings, and Booleans). This returns an integer.
-
- */
-
-  const serverCount = ns.scan();
-  let currentServer = 0;
-  let portBusters = 0;
-  {
-    /*Get the number of port busters we have */
-    if (ns.fileExists('brutessh.exe', 'home') == true) {
-      portBusters++
-      ns.tprint('BruteSSH.exe detected.')
-      if (ns.fileExists('ftpcrack.exe', 'home') == true) {
-        portBusters++
-        ns.tprint('FTPCrack.exe detected.')
-        if (ns.fileExists('relaysmtp.exe', 'home') == true) {
-          portBusters++
-          ns.tprint('RelaySMTP.exe detected.')
-          if (ns.fileExists('httpworm.exe', 'home') == true) {
-            portBusters++
-            ns, tprint('HTTPWorm.exe detected.')
-            if (ns.fileExists('sqlinject.exe', 'home') == true) {
-              portBusters++
-              ns.tprint('SQLInject.exe detected.')
-            }
-          }
-        }
+    //Get the amount of port buster programs on the home machine
+    {
+      if(ns.fileExists("BruteSSH.exe", "home") == true) {
+        bruteSSH = true;
+        ns.tprint("Found file [BruteSSH.exe] on machine [home]")
+      }
+      if(ns.fileExists("FTPCrack.exe", "home") == true) {
+        ftpCrack = true;
+        ns.tprint("Found file [FTPCrack.exe] on machine [home]")
+      }
+      if(ns.fileExists("relaySMTP.exe", "home") == true) {
+        relaySMTP = true;
+        ns.tprint("Found file [relaySMTP.exe] on machine [home]")
+      }
+      if(ns.fileExists("HTTPWorm.exe", "home") == true) {
+        httpWorm = true;
+        ns.tprint("Found file [HTTPWorm.exe] on machine [home]")
+      }
+      if(ns.fileExists("SQLInject.exe", "home") == true) {
+        sqlInject = true;
+        ns.tprint("Found file [SQLInject.exe] on machine [home]")
       }
     }
-    else {
-      ns.tprint('No port busters detected.')
-    }
-  }
 
-  if (ns.args.length != 0) {
-    
-    for (; currentServer < serverCount.length; currentServer++) {
+    for(; serverArray.length > currentServer; currentServer++) {
 
-      let host = serverCount[currentServer];
-
-      if (ns.getServerRequiredHackingLevel(host) <= ns.getHackingLevel()) {
-
-        if (ns.hasRootAccess(host) == false) {
-
-          let portsNeeded = ns.getServerNumPortsRequired(host);
-          ns.tprint('[' + host + '] requires [' + portsNeeded + '] ports open to run NUKE.exe');
-
-          if (portsNeeded <= portBusters) {
-
-            /* Open the ports of the target server */ {
-
-              if (portsNeeded > 0) {
-                ns.brutessh(host);
-
-                if (portsNeeded > 1) {
-                  ns.ftpcrack(host);
-
-                  if (portsNeeded > 2) {
-                    ns.relaysmtp(host);
-
-                    if (portsNeeded > 3) {
-                      ns.httpworm(host);
-
-                      if (portsNeeded > 4) {
-                        ns.sqlinject(host);
-
-                      }
-                    }
-                  }
-                }
-              }
-            }
-
-            try {
-              ns.nuke(host);
-            }
-
-            catch (error) {
-              ns.tprint('ERROR! NUKE.exe failed due to [' + error + ']');
-            }
-
-          }
-
-          else {
-
-            ns.tprint('Not enough port busters developed to nuke [' + host + '], skipping server.');
-            continue;
-
-          }
-
-        }
-
-        if(ns.args[0] == "-r" && ns.hasRootAccess(host) == true && ns.getServerMaxRam(host) >= ns.getScriptRam('/scripts/serverSetup.js')) {
-          
-          ns.scp("/scripts/serverSetup.js", host);
-          ns.exec("/scripts/serverSetup.js", host, 1, "-r");
-          
-        }
-
-      }
-      else {
-        ns.tprint('Player does not have a high enough hacking level for this [' + host + '], skipping server.');
+      try {ns.getServer(serverArray[currentServer])}
+      catch(error) {ns.tprint(error); continue;}
+      const targetProperties = ns.getServer(serverArray[currentServer]);
+      ns.tprint("Info: Target machine is [" + targetProperties.hostname + "]")
+      if(targetProperties.hostname === "home") {
+        ns.tprint("Warning! Target machine is home machine, skipping server.");
         continue;
       }
+      
+      if(targetProperties.requiredHackingSkill > ns.getHackingLevel()) {
+        ns.tprint("Warning! Target machine has a higher required hacking level than the player currently has, skipping server");
+        continue;
+      }
+      if(completedServers.includes(targetProperties.hostname)) {
+        ns.tprint("Warning! Target machine has already been hit by this program, skipping server");
+        continue;
+      }
+      if(targetProperties.sshPortOpen === false && bruteSSH === true) {
+        ns.brutessh(targetProperties.hostname);
+        ns.tprint("Ran ns.brutessh() on machine [" + targetProperties.hostname + "]");
+      }
+      if(targetProperties.ftpPortOpen === false && ftpCrack === true) {
+        ns.ftpcrack(targetProperties.hostname);
+        ns.tprint("Ran ns.ftpcrack() on machine [" + targetProperties.hostname + "]");
+      }
+      if(targetProperties.smtpPortOpen === false && relaySMTP === true) {
+        ns.relaysmtp(targetProperties.hostname);
+        ns.tprint("Ran ns.relaysmtp() on machine [" + targetProperties.hostname + "]");
+      }
+      if(targetProperties.httpPortOpen === false && httpWorm === true) {
+        ns.httpworm(targetProperties.hostname);
+        ns.tprint("Ran ns.httpworm() on machine [" + targetProperties.hostname + "]");
+      }
+      if(targetProperties.sqlPortOpen === false && sqlInject === true) {
+        ns.sqlinject(targetProperties.hostname);
+        ns.tprint("Ran ns.sqlinject() on machine [" + targetProperties.hostname + "]");
+      }
+      if(targetProperties.hasAdminRights === false) {
+        try {
+          ns.nuke(targetProperties.hostname);
+          ns.tprint("Attempting to run ns.nuke on machine [" + targetProperties.hostname + "]")
+        }
+        catch(error) {
+          ns.tprint("ERROR! Could not run ns.nuke() on machine [" + targetProperties.hostname + "] due to [" + error + "]");
+        }
+      }
+
+      completedServers.push(targetProperties.hostname);
+      serverArray.push(ns.scan(targetProperties.hostname));
+      serverArray = serverArray.filter((server) => server != "home")
+      ns.tprint("serverArray is currently at [" + serverArray + "]")
+
     }
-
-  }
-
-  else {
-
-    ns.tprint('ERROR! No arguments given! Required arguments are [1], [Is the program recursive]');
-    ns.tprint('|');
-    ns.tprint('Format is "run scripts/serverSetup.js (-r or any other string)"');
-
-  }
 }
