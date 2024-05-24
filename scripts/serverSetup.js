d82e85ab370f5a628bba342169fe4298520e145d
FemboyFireball <111195856+Fireball5939@users.noreply.github.com>
2024-05-24 12:50:43 -0500

    Fixed the weight function Changed some aspects of the weight function to work with existing variables and how the script deploys the hack script.

--- scripts/serverSetup.js
+++ scripts/serverSetup.js
@@ -97,7 +97,7 @@ export async function main(ns) {
   }
 
   function scanServers(fileInput) {
-    for (let highestWeight = []; serverArray.length > currentServer; currentServer++) {
+    for (let highestWeight = [0, "home"]; serverArray.length > currentServer; currentServer++) {
 
       const targetProperties = ns.getServer(serverArray[currentServer]);
 
@@ -155,34 +155,26 @@ export async function main(ns) {
           if (!server) return 0;
 
           // Don't ask, endgame stuff
-          if (server.startsWith('hacknet-node')) return 0;
+          if (server.hostname.startsWith('hacknet-node')) return 0;
+
+          // Skip home
+          if (server.hostname.startsWith('home')) return 0; ns.tprint(color.warn + "DEBUG: Skipping home for weight calculation");
 
           // Get the server information
           let so = server;
 
+          // Get the player information
+          let player = ns.getPlayer();
+
           // Set security to minimum on the server object (for Formula.exe functions)
           so.hackDifficulty = so.minDifficulty;
 
-          // We cannot hack a server that has more than our hacking skill so these have no value
-          if (so.requiredHackingSkill > ns.getHackingLevel()) return 0;
-
-          // Default pre-Formulas.exe weight. minDifficulty directly affects times, so it substitutes for min security times
-          let weight = so.moneyMax / so.minDifficulty;
-
-          // If we have formulas, we can refine the weight calculation
-          if (ns.fileExists('Formulas.exe')) {
-            // We use weakenTime instead of minDifficulty since we got access to it, 
-            // and we add hackChance to the mix (pre-formulas.exe hack chance formula is based on current security, which is useless)
-            weight = so.moneyMax / ns.formulas.hacking.weakenTime(so, player) * ns.formulas.hacking.hackChance(so, player);
-          }
-          else
-            // If we do not have formulas, we can't properly factor in hackchance, so we lower the hacking level tolerance by half
-            if (so.requiredHackingSkill > ns.getHackingLevel() / 2)
-              return 0;
-
+          // We use weakenTime instead of minDifficulty since we got access to it, 
+          // and we add hackChance to the mix (pre-formulas.exe hack chance formula is based on current security, which is useless)
+          let weight = so.moneyMax / ns.formulas.hacking.weakenTime(so, player) * ns.formulas.hacking.hackChance(so, player);
           return weight;
-        }
-        serverWeight = Weight(ns, targetProperties);
+          }
+        let serverWeight = Weight(ns, targetProperties);
         if (highestWeight[0] < serverWeight) {
           highestWeight = [serverWeight, targetProperties.hostname];
         }
@@ -201,8 +193,9 @@ export async function main(ns) {
         }
 
         if (maxThreads > 0) {
+          let highestWeightServer = ns.getServer(highestWeight[1])
           ns.tprint(color.info + "Info: Running program [" + fileInput + "] on machine [" + targetProperties.hostname + "] with thread count [" + maxThreads + "]");
-          ns.exec(fileInput, targetProperties.hostname, maxThreads, highestWeight[1], targetProperties.minDifficulty, targetProperties.moneyMax);
+          ns.exec(fileInput, targetProperties.hostname, maxThreads, highestWeightServer.hostname, highestWeightServer.minDifficulty, highestWeightServer.moneyMax);
         }
 
       }
